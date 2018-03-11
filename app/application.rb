66404df6703269bdb6fbeda627a5ea741cb4ba65
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-11 23:22:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       if req.path.match(/cart/)
         @@cart.each do |item|
           resp.write "#{item}\n"
+        end 
       else
         resp.write "Your cart is empty"
           #empty cart messge
