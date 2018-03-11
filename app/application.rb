1bd7e9c3e12eeeaf65dc1a34c198494ddf6605f0
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-11 23:05:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,11 +10,11 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      end 
+      end
       elsif req.path.match(/cart/)
         @@cart.each do |item|
           resp.write "#{item}\n"
-        end 
+        end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
