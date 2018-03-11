2a22726036b06e75a00f7eac96e9400a0a2ca2a7
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-11 23:02:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,11 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item 
+      end 
     else
       resp.write "Path Not Found"
     end
