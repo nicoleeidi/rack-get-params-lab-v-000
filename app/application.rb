e30366dfbf42af89d3043a878d77029d626e58b9
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-12 01:12:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
       search_term = req.params["item"]
     if @@items.include?(search_term)
         @@cart << search_term
-      end 
+      end
      else
       resp.write "Path Not Found"
     end
